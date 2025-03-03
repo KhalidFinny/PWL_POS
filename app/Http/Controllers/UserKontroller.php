743285adee76d1fd00491e5c2ac66812b6bdecab
finny <katthoriq@gmail.com>
2025-03-03 09:31:09 +0700

    Praktikum 2 Retrieve single models

--- app/Http/Controllers/UserKontroller.php
+++ app/Http/Controllers/UserKontroller.php
@@ -10,15 +10,9 @@ class UserKontroller extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('2345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function ()
+    {abort(404);
+    });
         return view('user', ['data' => $user]);
 
     }
